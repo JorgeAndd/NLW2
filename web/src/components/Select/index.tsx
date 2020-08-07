c4cc031ee8c9966c2b6b90eb1dcb7eb6bd9c093b
JorgeAndd <jorgeluizandrade@outlook.com>
2020-08-07 10:41:59 -0300

    Creating Select component

---
+++ web/src/components/Select/index.tsx
@@ -0,0 +1,31 @@
+import React, { SelectHTMLAttributes } from 'react';
+
+import './styles.css';
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  name: string;
+  label: string;
+  options: Array<{
+    value: string;
+    label: string;
+  }>;
+}
+
+const Select: React.FC<SelectProps> = (props) => {
+  const { label, name, options, ...otherProps } = props;
+
+  return (
+    <div className="select-block">
+      <label htmlFor={name}>{label}</label>
+      <select id={name} {...otherProps}>
+        <option value="" disabled selected hidden>Selecione uma opção</option>
+
+        {options.map(o => {
+          return <option key={o.value} value={o.value} label={o.label}></option>
+        })}
+      </select>
+    </div>
+  );
+}
+
+export default Select;
