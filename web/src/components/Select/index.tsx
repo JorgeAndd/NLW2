bb54d61d97118060fdb9bb3e6c3e3462b58b1a00
JorgeAndd <jorgeluizandrade@outlook.com>
2020-08-08 19:08:36 -0300

    Fixing warn of Select component again

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -17,7 +17,7 @@ const Select: React.FC<SelectProps> = (props) => {
   return (
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
-      <select defaultValue="" id={name} {...otherProps}>
+      <select value="" id={name} {...otherProps}>
         <option value="" disabled hidden>Selecione uma opção</option>
 
         {options.map(o => {
