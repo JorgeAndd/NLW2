cde16abb9f404ead2a1d852c203398de557411d2
JorgeAndd <jorgeluizandrade@outlook.com>
2020-08-08 17:34:11 -0300

    Adding scheduleItems on TeacherForm page

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
@@ -10,6 +10,21 @@ import warningIcon from '../../assets/images/icons/warning.svg';
 import './styles.css';
 
 function TeacherForm() {
+  const [scheduleItems, setScheduleItem] = useState([
+    { week_day: 0, from: '', to: '' }
+  ]);
+
+  function addNewScheduleItem() {
+    setScheduleItem([
+      ...scheduleItems,
+      {
+        week_day: 0,
+        from: '',
+        to: ''
+      }
+    ])
+  }
+
   return (
     <div id="page-teacher-form" className="container">
       <PageHeader
@@ -55,8 +70,29 @@ function TeacherForm() {
               <button type="button" onClick={addNewScheduleItem}>
                 + Novo horário
               </button>
-              </span>
+            </span>
           </legend>
+
+          {scheduleItems.map(i => (
+            <div key={i.week_day} className="schedule-item">
+              <Select
+                name="week-day"
+                label="Dia da semana"
+                options={[
+                  { value: '0', label: 'Domingo' },
+                  { value: '1', label: 'Segunda-feira' },
+                  { value: '2', label: 'Terça-feira' },
+                  { value: '3', label: 'Quarta-feira' },
+                  { value: '4', label: 'Quinta-feira' },
+                  { value: '5', label: 'Sexta-feira' },
+                  { value: '6', label: 'Sábado' },
+                ]}
+              />
+
+              <Input name="from" label="Das" type="time" />
+              <Input name="to" label="Até" type="time" />
+            </div>
+          ))}
         </fieldset>
 
         <footer>
