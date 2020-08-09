848b0fc1f34609c8f950976d1f4ef7a28c709761
JorgeAndd <jorgeluizandrade@outlook.com>
2020-08-09 14:37:12 -0300

    Navigation structure for Study with tabs

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,7 @@
+import { View } from 'react-native';
+
+function Favorites() {
+  return <View />
+}
+
+export default Favorites;
---
+++ mobile/src/pages/TeacherList/index.tsx
@@ -0,0 +1,7 @@
+import { View } from 'react-native';
+
+function TeacherList() {
+  return <View />
+}
+
+export default TeacherList;
