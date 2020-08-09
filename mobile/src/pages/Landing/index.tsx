d9560c49c04becc461e6869d26387eb3519bfa05
JorgeAndd <jorgeluizandrade@outlook.com>
2020-08-09 11:12:41 -0300

    Starting basic styling of Landing page

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Text } from 'react-native';
 
 import styles from './styles';
 
+import landingImg from '../../assets/images/landing.png';
+
 function Landing() {
   return (
-    <View>
+    <View style={styles.container}>
+      <Image source={landingImg} style={styles.banner} />
+
+      <Text style={styles.title}>
+        Seja bem-vindo, {'\n'}
+        <Text style={styles.titleBold}>O que deseja fazer?</Text>
+      </Text>
     </View>
   );
 }
