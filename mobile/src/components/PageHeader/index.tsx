24f4356b4881dbaead4acde8556d77c377af8d4a
JorgeAndd <jorgeluizandrade@outlook.com>
2020-08-10 16:40:08 -0300

    Fixing non obligatory field on PageHeader

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -10,7 +10,7 @@ import styles from './styles';
 
 interface PageHeaderProps {
   title: string;
-  headerRight: ReactNode;
+  headerRight?: ReactNode;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
